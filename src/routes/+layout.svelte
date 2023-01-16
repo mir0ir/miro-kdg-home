e79d7421d0d1d3df183924ed2c76c368d5ad16ed
raremiroir <mirostorm@gmail.com>
2023-01-16 15:37:09 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	import "../app.postcss";
+    import Header from './Header.svelte';
+    import './styles.css';
+</script>
+
+<div class="app flex flex-col min-h-screen bg-black max-w-screen overflow-x-hidden">
+	<header></header>
+
+	<main 
+		class="
+			box-border
+			flex flex-col flex-1 
+			p-4 m-8 mb-4 
+			border-4 border-dashed border-green-500
+			">
+		<slot></slot>
+	</main>
+
+	<footer class="text-gray-100 flex flex-col justify-center items-center p-3">
+		<!-- <p>Visit <a href="https://kit.svelte.dev" class="font-bold">kit.svelte.dev</a> to learn SvelteKit</p> -->
+	</footer>
+</div>
+
+<style>
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import KdgSvg from '../lib/components/Media/KdgSvg.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Miro Polfliet KdG" />
+</svelte:head>
+
+<section 
+	class='
+		text-green-500 w-full h-full
+		flex items-center flex-col justify-center'>
+	<h1>
+		Welcome to my Website <br/>
+		(Work in Progress)
+	</h1>
+
+	<h2>
+		by <strong>Miro Storm Polfliet</strong>
+	</h2>
+
+	<KdgSvg/>
+</section>
